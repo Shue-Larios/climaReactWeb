b4d53b4f1939d2dd20b7ab171ce858fb7d85238c
Rene Larios <112102394+Shue-Larios@users.noreply.github.com>
2023-05-20 00:27:30 -0600

    App modificado
    colocado advertencia del modo desarrollador

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import { Clima } from './Clima'
-import viteLogo from '/vite.svg'
+
 
 
 function App() {
 
+  const [isDeveloperMode, setIsDeveloperMode] = useState(false);
+
+ useEffect(() => {
+    const detectDeveloperMode = () => {
+      const devtools = /./;
+      devtools.toString = () => {
+        setIsDeveloperMode(true);
+        // para mostrar el msj en el log
+        console.log(
+          '%c⚠️ Modo desarrollador activado',
+          'color: white; background-color: red; font-size: 24px; padding: 8px;'
+        );
+      };
+      console.log('%c', devtools);
+    };
+
+    detectDeveloperMode();
+  }, []);
+
+
+
+
   return (
    <>
+    <div>
+       {isDeveloperMode && (
+         <div className="developer-mode-ad">
+           <p>¡Modo desarrollador activado!</p>
+           <p>Este es un anuncio para los desarrolladores.</p>
+         </div>
+       )}
+       {/* Resto del contenido de tu aplicación */}
+     </div>
    <Clima />
    </>
   )
